bbcbfc4e982b9a72d6811d5eadaffeeb0c9732fa
= <halildrk@gmail.com>
2022-07-26 00:38:48 +0300

    Update Day-1 according to given reviews

--- day-1/guess_the_number.zig
+++ day-1/guess_the_number.zig
@@ -1,41 +1,47 @@
 const std = @import("std");
 const Random = std.rand.DefaultPrng;
 const Instant = std.time.Instant;
-const stdin = std.io.getStdIn().reader();
-const stdout = std.io.getStdOut().writer();
 const fmt = std.fmt;
 const print = std.debug.print;
-const page_allocator = std.heap.page_allocator;
 
 pub fn main() !void {
+  const stdin = std.io.getStdIn().reader();
+  const stdout = std.io.getStdOut().writer();
+
+  // initialize the allocator
+  var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+  const allocator = gpa.allocator();
+
+  // deinitialize the allocator
+  defer _ = gpa.deinit();
+
   // use current nanosec for rand seed
   var instant = try Instant.now();
   const seed: u64 = @intCast(u64, instant.timestamp.tv_nsec);
 
   // generate a random number
   var prng = Random.init(seed);
-  const rand = &prng.random();
-  const magic_number: i32 = rand.intRangeAtMost(i32, 0, 100);
+  const magic_number: i32 = prng.random().intRangeAtMost(i32, 0, 100);
 
   // game loop
   while (true) {
 
     // read user input
     const till_line = try stdin.readUntilDelimiterAlloc(
-      page_allocator,
+      allocator,
       '\n',
       1024,
     );
 
     // free it when it got out of the scope
-    defer page_allocator.free(till_line);
-    
+    defer allocator.free(till_line);
+
     const line = std.mem.trim(u8, till_line, "\r");
 
     // get integer value from user input
     var guess = fmt.parseInt(i32, line, 0) catch |err| switch (err) {
       error.Overflow => {
-        print("buffer overflow!\n", .{});
+        print("integer overflow!\n", .{});
         continue;
       },
 
